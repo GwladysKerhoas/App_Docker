1f2ffe2a7dbf8c32fc7a8196c48915f53467043b
Gwladys Kerhoas <73121667+GwladysKerhoas@users.noreply.github.com>
2022-03-02 17:19:05 +0100

    Modif2
    Modif2

--- appli_streamlit2.py
+++ appli_streamlit2.py
@@ -18,7 +18,7 @@ import plotly.graph_objects as go
 
 
 # Titre et sous-titre
-st.title("Atelier Docker M2 SISE 2022")
+st.title("Atelier Docker M2 SISE 2022 TEST")
 st.subheader("Exploration des données des résultats des étudiants de deux écoles portuguaise")
 
 file = st.file_uploader("Chargement du fichier")
